c5a19781091660d26c6318843b8f3b03dc7b47ca
jesakim <jesakim1@gmail.com>
2022-09-12 15:56:59 +0100

    puching loops challenges

--- Boucle/Challenge 5.c
+++ Boucle/Challenge 5.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Ecrire un programme C qui lit un entier et l'affiche inversé.
+On choisira de ne pas afficher chiffre par chiffre mais de construire l'entier inversé puis l'afficher.*/
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int nbr, inverse;
+    inverse = 0;
+    printf("veuillez entrer un nombre: ");
+    scanf("%d", &nbr);
+
+    while(nbr!=0)
+    {
+        inverse = (inverse*10) + (nbr%10);
+        nbr = nbr/10;
+    }
+    printf("L'inverse est: %d\n", inverse);
+
 }
