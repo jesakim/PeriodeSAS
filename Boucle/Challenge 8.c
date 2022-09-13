63011cb549de69ec0f6a440617c2889dcbfac868
jesakim <jesakim1@gmail.com>
2022-09-13 15:06:29 +0100

    période SAS

--- Boucle/Challenge 8.c
+++ Boucle/Challenge 8.c
@@ -2,15 +2,13 @@
 #include<stdlib.h>
 int main()
 {
-    float a,b,m,fa,fb,fm;
-    a=-15;
-    b=-10;
+    float a=-15,b=-10,m,fa,fm;
     while(b-a>0.000001)
     {
-       m=a+((b-a)/2);
+       m=(b+a)/2;
        fa=a*a*a+12*a*a+1;
        fm=m*m*m+12*m*m+1;
-       if(fa*fm<=0) b=m;
+       if(fa*fm<=0)b=m;
        else a=m;
     }
     printf("la solution de l'équation x3+12x2+1=0 dans l'intervalle [-15,-10] est :%.5f\n",a);
