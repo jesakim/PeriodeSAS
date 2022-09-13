e7d263009fa1782f5b02c1bf7a8c9c4dc2dd495e
jesakim <jesakim1@gmail.com>
2022-09-13 10:18:04 +0100

    Periode SAS

--- Boucle/Challenge 7.c
+++ Boucle/Challenge 7.c
@@ -4,7 +4,9 @@
 int main()
 {
     int fibo(a){
-    if(a==0 || a==1)return 1;
+    if(a==2 || a==1)return 1;
+    else if(a==0)return 0;
+
     else return fibo(a-1)+fibo(a-2);
     }
     int num;
