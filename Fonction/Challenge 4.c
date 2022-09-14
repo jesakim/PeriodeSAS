2171ea6cab095181707d6fb31be9807845ffc1ec
jesakim <jesakim1@gmail.com>
2022-09-14 15:49:34 +0100

    functions challenges

--- Fonction/Challenge 4.c
+++ Fonction/Challenge 4.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main()
-{
-    int divededby(int n,int a){
-        return n/a;
+bool isPremier(int a){
+        int b=0;
+    for (int i=2;i<a;i++){
+        if(a%i==0)b++;
     }
+    if(b==0)return true;
+    else return false;
+}
+int divededby(int n,int a){
+        if(isPremier(n) && a!=0)printf("%d / %d = %d",n,a,n/a);
+        else printf("vérifier que le numérateur est premier et le dénominateur non nul.");
+    }
+int main(){
 }
---
+++ Fonction/Challenge 5.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void triFusion(int i, int j, int tab[], int tmp[]) {
+    if(j <= i)return;
+
+    int m =(i + j)/2;
+
+    triFusion(i,m,tab,tmp);
+    triFusion(m +1,j,tab,tmp);
+
+    int pg=i;
+    int pd=m+1;
+    int c;
+    for(c=i;c<=j;c++) {
+        if(pg==m+1){
+            tmp[c]=tab[pd];
+            pd++;
+        }else if (pd==j+1) {
+            tmp[c]=tab[pg];
+            pg++;
+        }else if (tab[pg]<tab[pd]) {
+            tmp[c]=tab[pg];
+            pg++;
+        }else {
+            tmp[c]=tab[pd];
+            pd++;
+        }
+    }
+
+    for(c=i;c<=j;c++){
+        tab[c]=tmp[c];
+    }
+}
+
+
+int main(){
+  int nbr,i,tab[100],tmp[100];
+
+  printf("Entrez le nombre d'éléments dans le tableau:");
+  scanf("%d",&nbr);
+
+  printf("Entrez %d entiers :",nbr);
+
+  for (i=0;i<nbr;i++)
+    scanf("%d",&tab[i]);
+    printf("votre tableau non trier est :");
+    for (i=0;i<nbr;i++)printf("%d ",tab[i]);
+    triFusion(0,nbr-1,tab,tmp);
+
+    printf("\nvotre tableau non trier est :");
+    for(i=0;i<nbr;i++)  {
+        printf("%d ",tab[i]);
+  }
+  return 0;
+}
+
